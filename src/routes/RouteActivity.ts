96f62455cf191ec7596589162e69d8866124f39f
senpaiharde <xxslavan1@gmail.com>
2025-05-22 18:08:51 +0300

    updated acitivity route

--- src/routes/RouteActivity.ts
+++ src/routes/RouteActivity.ts
@@ -2,14 +2,14 @@ import { Router, Request, Response } from 'express';
 import { authMiddleware } from '../middlewares/authmiddleware';
 import Activity from '../models/activity';
 import { ObjectId } from 'mongoose';
-import User                         from '../models/User';
+import User from '../models/User';
 import mongoose from 'mongoose';
 const router = Router();
 router.use(authMiddleware);
 
 interface ActivityDTO {
   _id: ObjectId;
-  user: { _id: ObjectId; fullName: string; avatar?: string };
+  user: { _id: ObjectId; fullname: string; avatar?: string };
   entity: { kind: string; id: ObjectId };
   action: string;
   payload?: Record<string, unknown>;
@@ -28,20 +28,22 @@ router.get('/:taskId', async (req: Request, res: Response): Promise<any> => {
       'entity.kind': 'task',
     })
       .sort({ createdAt: -1 })
-      .select('id user entity action payload createdAt')
-      .populate({ path: 'user', select: 'fullName avatar' })
+      .select('_id user entity action payload createdAt')
+      .populate({ path: 'user', select: 'fullname email avatar' })
       .lean<ActivityDTO[]>();
 
-    const result = activities.map((d) => ({
-      id: d._id.toString(),
-      userId: d.user._id.toString(),
-      userName: d.user.fullName,
-      userAvatar: d.user.avatar || null,
-      entityId: d.entity.id.toString(),
-      action: d.action,
-      payload: d.payload,
-      createdAt: d.createdAt.toISOString(),
-    }));
+    const result = activities
+      .filter((d) => d.user)
+      .map((d) => ({
+        id: d._id.toString(),
+        userId: d.user._id.toString(),
+        userName: d.user.fullname,
+        userAvatar: d.user.avatar || null,
+        entityId: d.entity.id.toString(),
+        action: d.action,
+        payload: d.payload,
+        createdAt: d.createdAt.toISOString(),
+      }));
 
     return res.json(result);
   } catch (err) {
